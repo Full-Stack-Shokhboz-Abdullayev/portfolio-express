2da54c2caec1fba8b8ac64f6032a834ad24b8e17
Shokhboz Abdullaev <abdullaevadiana27@gmail.com>
2021-02-14 12:19:23 +0300

    'client-deattached2'

--- client/README.md
+++
@@ -1,6 +0,0 @@
-### Portfolio V2
-
-Not responsive yet.
-Best experience with Chrome.
-
-You can see it [here](https://shokhboz-portfoliov2/).
--- client/src/assets/jsComponents/validate.js
+++
@@ -1,9 +0,0 @@
-export default function(e) {
-    const val = e.target.value;
-    
-    if (val === '' || val === null) {
-        e.target.parentElement.classList.remove('filled');
-    } else {
-        e.target.parentElement.classList.add('filled');
-    }
-}
